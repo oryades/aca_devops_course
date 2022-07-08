5f4090b538ddb340b5e1c93173877eb72bde780a
Hakob <hmnacakanyan@gmail.com>
2022-07-08 16:49:43 +0400

    Erased all

--- users/gaya/date_msgs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-log_message() {
-	echo $(date +"%F %R - ") $1
-}
-
-log_message 'well hi there'
-log_message 'alright, bye'
--- users/robert.poghosyan/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-log_message()  {
-       echo $(date '+%Y-%m-%d %H:%M') " - " $@
-}       
-
-log_message "Hello Message"
-
-log_message "Goodby Message"
