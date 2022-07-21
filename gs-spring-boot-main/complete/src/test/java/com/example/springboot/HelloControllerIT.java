4fa10d6d9376226664e34fa43a638972117eb30e
Artyom Krilov <oryades@gmail.com>
2022-07-21 21:23:26 +0400

    Added from developer

--- gs-spring-boot-main/complete/src/test/java/com/example/springboot/HelloControllerIT.java
+++ gs-spring-boot-main/complete/src/test/java/com/example/springboot/HelloControllerIT.java
@@ -18,6 +18,6 @@ public class HelloControllerIT {
     @Test
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity("/", String.class);
-        assertThat(response.getBody()).isEqualTo("Hello world!");
+        assertThat(response.getBody()).isEqualTo("Hello world!(Artyom)");
     }
 }
