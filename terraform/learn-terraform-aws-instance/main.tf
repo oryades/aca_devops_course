b5f2d27b71a5fd8ae649449eea795b378b7250d9
Artyom Krilov <artyom.krilov@mentorcliq.com>
2022-09-06 20:38:00 +0400

    added s3

--- terraform/learn-terraform-aws-instance/main.tf
+++ terraform/learn-terraform-aws-instance/main.tf
@@ -22,3 +22,11 @@ resource "aws_instance" "app_server" {
   }
 }
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
